b5a499774e3b97bc4b1c51ae3d9d588b3c0e4850
xjackhammered <hammerofjack7@gmail.com>
2025-07-22 17:41:04 +0600

    modified views and models

--- backend/api/models.py
+++ backend/api/models.py
@@ -18,7 +18,7 @@ class Movie(models.Model):
 
 class Review(models.Model):
     content = models.TextField(max_length=1000, null=False)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews")
     rating = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
