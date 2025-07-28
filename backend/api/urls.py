3ed1b2dc2ef2fceea718d33eba04765aba8c0f34
xjackhammered <hammerofjack7@gmail.com>
2025-07-28 18:08:15 +0600

    added views for views and genres

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
   path('movie-update/<int:id>/', views.movieUpdate, name="movie-update"),
   path('movie-add/', views.addMovie, name="add-movie"),
   path('movie-delete/<int:id>/', views.deleteMovie, name="delete-movie"),
+  path('genre-add/', views.genreAdd, name="add-genre"),
+  path('genre-delete/<int:id>/',views.genreDelete, name="delete-genre"),
+  path('genres/', views.genreList, name="genres"),
+  path('review-add/', views.reviewAdd),
+  path('review-delete/<int:id>/',views.reviewDelete),
+  path('reviews/', views.reviewList),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from .serializers import MovieSerializer
+from .serializers import MovieSerializer, GenreSerializer, ReviewSerializer
 from rest_framework.decorators import api_view
 from .models import Movie, Genre, Review
 
@@ -45,3 +45,51 @@ def deleteMovie(request, id):
     movie.delete()
 
     return Response("Movie deleted successfully!")
+
+@api_view(['POST'])
+def genreAdd(request):
+    serializer = GenreSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+    
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def genreDelete(request, id):
+    genre = Genre.objects.get(id=id)
+    genre.delete()
+
+    return Response("Genre successfully deleted")
+
+@api_view(['GET'])
+def genreList(request):
+    genre = Genre.objects.all()
+    serializer = GenreSerializer(genre, many=True)
+    
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def reviewList(request):
+    review = Review.objects.all()
+    serializer = ReviewSerializer(review, many=True)
+    
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def reviewDelete(request, id):
+    review = Review.objects.get(id=id)
+    review.delete()
+
+    return Response("Review successfully deleted")
+
+@api_view(['POST'])
+def reviewAdd(request):
+    serializer = ReviewSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+    
+    return Response(serializer.data)
